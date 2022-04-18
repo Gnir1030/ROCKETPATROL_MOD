6ff3bdd5e8103c937e50dbf1ce4c9aabde534a6f
Louis Lim <llim3@ucsc.edu>
2022-04-18 00:39:46 -0700

    Merge branch 'master' of https://github.com/Gnir1030/ROCKETPATROL_MOD

--- src/main.js
+++ src/main.js
@@ -19,3 +19,18 @@ let keyA, keyD, keyW, keyR, keyLEFT, keyRIGHT, keyUP, keyDOWN;
 localStorage.setItem("highStorage", 0); //beginnermode_HighScoreStorage
 localStorage.setItem("highStorage2", 0); //expertmode_HighScoreStorage
 
+/*
+Track a high score that persists across scenes and display it in the UI (5), high scores for each mode are saved in separated storages
+Implement the 'FIRE' UI text from the original game (5)
+Add your own (copyright-free) background music to the Play scene (5) , and restart with playscene
+Implement the speed increase that happens after 30 seconds in the original game (5)
+Create a new scrolling tile sprite for the background (5)
+Allow the player to control the Rocket after it's fired (5)
+
+Create 4 new explosion SFX and randomize which one plays on impact (10)
+Display the time remaining (in seconds) on the screen (10)
+Create a new title screen (e.g., new artwork, typography, layout) (10)
+
+Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+Implement an alternating two-player mode (20)
+*/
